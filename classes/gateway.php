7c91c92f7e02d104c11f12979a12c01af53f3d8e
Santosh Nagargoje <santosh.nag2217@gmail.com>
2024-11-15 03:41:47 +0530

    Fixed code checker issue

--- classes/gateway.php
+++ classes/gateway.php
@@ -37,7 +37,7 @@ class gateway extends \core_payment\gateway {
      */
     public static function get_supported_currencies(): array {
         // 3-character ISO-4217: https://en.wikipedia.org/wiki/ISO_4217#Active_codes.
-        return ['INR',];
+        return ['INR'];
     }
 
     /**
